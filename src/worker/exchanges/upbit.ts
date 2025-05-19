2998a9ecb2b9ef8b48d9597ded0daf4f1f093b9b
Raymond <raymondtheboss1@gmail.com>
2025-05-19 12:00:11 -0400

    Add upbit subscription

---
+++ src/worker/exchanges/upbit.ts
@@ -0,0 +1,104 @@
+import Exchange from '../exchange'
+
+export default class UPBIT extends Exchange {
+  id = 'UPBIT'
+  private locallySubscribedPairs = new Set<string>();
+
+  protected endpoints = {
+    PRODUCTS: 'https://api.upbit.com/v1/market/all'
+  }
+
+  async getUrl() {
+    return 'wss://api.upbit.com/websocket/v1'
+  }
+
+  formatProducts(data) {
+    return data.map(product => product.market)
+  }
+
+  /**
+   * Sub
+   * @param {WebSocket} api
+   * @param {string} pair
+   */
+  async subscribe(api, pair) {
+    if (!(await super.subscribe(api, pair))) {
+      return false;
+    }
+
+    this.locallySubscribedPairs.add(pair);
+
+    api.send(
+        JSON.stringify([
+            { ticket: 'aggr' },
+            { type: 'trade', codes: Array.from(this.locallySubscribedPairs) },
+            { format: 'DEFAULT' }
+        ])
+    );
+
+    return true;
+  }
+
+  /**
+   * Sub
+   * @param {WebSocket} api
+   * @param {string} pair
+   */
+  async unsubscribe(api, pair) {
+    if (!(await super.unsubscribe(api, pair))) {
+      return false;
+    }
+
+    this.locallySubscribedPairs.delete(pair);
+
+    // Per requirements, do not send an unsubscribe message to Upbit.
+    // If we needed to inform the server to stop sending for this pair,
+    // we would send an updated subscription list (similar to subscribe)
+    // or handle it differently if Upbit had specific 'stop' mechanisms.
+    // For now, local filtering in onMessage will handle unwanted messages.
+
+    return true;
+  }
+
+  formatTrade(trade) {
+    return {
+      exchange: this.id,
+      pair: trade.code,
+      timestamp: trade.trade_timestamp,
+      price: +trade.trade_price,
+      size: +trade.trade_volume,
+      side: trade.ask_bid
+    }
+  }
+
+  onMessage(event, api) {
+    let tradeData;
+    try {
+      tradeData = JSON.parse(event.data);
+    } catch (e) {
+      console.warn(`[${this.id}] Failed to parse JSON message:`, event.data, e);
+      return;
+    }
+
+    if (
+      !tradeData ||
+      tradeData.type !== 'trade' ||
+      tradeData.stream_type !== 'REALTIME' ||
+      !this.locallySubscribedPairs.has(tradeData.code)
+    ) {
+      // Log or handle non-trade/malformed messages or non-subscribed pairs if necessary.
+      // console.debug(`[${this.id}] Received non-trade message, unexpected data, or non-subscribed pair:`, event.data);
+      return;
+    }
+
+    return this.emitTrades(api.id, [this.formatTrade(tradeData)]);
+  }
+
+  onApiCreated(api) {
+    this.startKeepAlive(api, 'PING', 60000)
+  }
+
+  onApiRemoved(api) {
+    this.stopKeepAlive(api)
+  }
+}
